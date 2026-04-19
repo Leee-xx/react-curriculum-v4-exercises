51682fbce8e331f869b77c6f5cc0249a90739537
Lee Wang <leeeee@gmail.com>
2026-04-19 13:39:41 -0700

    Lesson 3

--- src/exercises/lesson-03/studentWork.jsx
+++ src/exercises/lesson-03/studentWork.jsx
@@ -2,11 +2,16 @@
 //Exercise: React Bug Hunt – Fix the broken components in this folder
 //Impport components here
 
+import BugEffectLoop from './BugEffectLoop.jsx';
+import BugMutatedState from './BugMutatedState.jsx';
+import BugProps from './BugProps.jsx';
+
 export default function StudentWork() {
   return (
     <div>
-      {/* add components here */}
-      <p>Student output will go here</p>
+      <BugEffectLoop />
+      <BugMutatedState />
+      <BugProps />
     </div>
   );
 }
