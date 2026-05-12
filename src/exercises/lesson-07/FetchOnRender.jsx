56a3427d45251a8ca5029f09d433b16c30d3f1cb
Lee Wang <leeeee@gmail.com>
2026-05-12 15:20:13 -0700

    wip

--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
+
+import { getPosts } from './api.js';
 import './Lesson07Styles.css';
 
 export default function FetchOnRender() {
+  useEffect(() => {
+    (async () => {
+      try {
+        const resp = await getPosts();
+        console.log(resp);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
   return (
     <div className="root">
       <h1 className="heading">Fetch list of posts on render</h1>
